79d575e8776bdae64379de975b3bbb8a4ce99b5f
Frederick MacGregor <frederickmacgregor@Fredericks-MacBook-Pro.local>
2022-08-11 15:23:56 +0100

    add functionality for PWM

--- index.js
+++ index.js
@@ -4,17 +4,48 @@ import rpio from 'rpio';
 const PORT = 3050;
 const app = express();
 
-const pin = 11;
+const pin = 12;
 const rotation = 0.0030;
+const clockdiv = 256;
+const range = 1500;
+const PWM = process.env.PWM ?? false;
 
+const setupPWM = () => {
+	rpio.open(pin, rpio.PWM);
+	rpio.pwmSetClockDivider(clockdiv);
+	rpio.pwmSetRange(pin, range);
+	rpio.pwmSetData(pin, 0);
+};
 
-app.get('/activate', (_, res) => {
-	console.log('Activating');
+const moveSimple = () => {
 	rpio.open(pin, rpio.OUTPUT, rpio.LOW);
 	rpio.write(pin, rpio.HIGH);
 	rpio.sleep(rotation);
 	rpio.write(pin, rpio.LOW);
-	res.sendStatus(200);
+};
+
+const movePWM = () => {
+	rpio.pwmSetData(pin, 7);
+	rpio.sleep(2.5);
+	rpio.pwmSetData(pin, 0);
+};
+
+if(PWM) {
+	setupPWM();
+}
+
+
+app.get('/activate', (_, res) => {
+	console.log('Activating');
+	if (PWM) {
+		movePWM();
+	} else {
+		moveSimple();
+	}
+
+
+	res.status(200).send("Mouse destroyed \n");
+	res.set
 })
 
 
